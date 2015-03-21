539b7d8044c06d25dc5591147ca186677fb7c2fb
Jordan Acosta <jordan@solfo.com>
2015-03-21 15:17:05 -0700

    Set up basic app and api infrastructure

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
 'use strict';
 
-var express = require('express'),
+global.rekuire = require('rekuire');
+
+var app = require('express')(),
     reactEngine = require('express-react-views').createEngine(),
-
-    apiRouter = require('./lib/api-router'),
-
+    apiRouter = rekuire('lib/api'),
     logger = function( req, res, next ) {
         console.log( 'req: ', req );
         next();
     },
-    handleHome = function( req, res ) {
+    handleApp = function( req, res ) {
         res.render( 'index' );
-    });
+    };
 
 app
-    // setup render engine
     .set( 'view engine', 'jsx' )
     .engine( 'jsx', reactEngine )
-
-    // app stuff
     .use( logger )
     .use( '/api', apiRouter )
     .get( '/', handleApp )
-
-    // start
     .listen( '8000' );
 
 console.log( 'the app listens on port 8000' );
