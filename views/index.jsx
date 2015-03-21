2dd560714607e27c275baf1bcda4eb5b440fbcce
Jordan Acosta <jordan@solfo.com>
2015-03-21 15:26:02 -0700

    Couple of bug fixes

--- views/index.jsx
+++ views/index.jsx
@@ -1,8 +1,8 @@
 'use strict';
 
-var R = require('react');
+var React = require('react');
 
-module.exports = R.createClass({
+module.exports = React.createClass({
     render: function(){
         // can we get rid of this, and make 
         // just like mustache, where we don't
@@ -15,9 +15,6 @@ module.exports = R.createClass({
             <html>
                 <head>
                     <title>{p.message}</title>
-                    // should we lock the scale at 1.0 for all of our apps?
-                    // if we're serious about mobile apps, then we our users
-                    // shouldn't have to zoom
                     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 </head>
                 <body>
