9d3d2475d81c48924ad0927f63045f8ee9c849fe
Jordan Acosta <jordan@solfo.com>
2015-03-21 22:07:34 -0700

    App starts up

--- js/init.js
+++ js/init.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log( 'INIT' );
+console.log( 'STARTING APP' );
+
+var React = require('react'),
+    app = React.createElement( require('./app') ),
+    rendered = function() {
+        console.log( 'APP STARTED' );
+    };
+
+React.render(
+    app,
+    document.body,
+    rendered
+);
